0c0cdfcc777d2fae1e6a71de96d918816aa2ec31
mikemickey26 <107206773+mikemickey26@users.noreply.github.com>
2022-07-19 08:16:41 -0700

    updated

---
+++ index.js
@@ -0,0 +1,9 @@
+//Write a program to check if a number is an even number or an odd number
+
+let number = 6
+let remainder = (number % 2)
+if (remainder == 0){
+    console.log('${number} is even')
+}else{
+    console.log('${number} is odd')
+}
