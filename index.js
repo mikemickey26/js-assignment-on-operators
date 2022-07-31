f2a15f7932ee9035bce274defacb9e7343c4de05
mikemickey26 <107206773+mikemickey26@users.noreply.github.com>
2022-07-31 06:34:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 //Write a program to check if a number is an even number or an odd number
 
-let number = 6
-let remainder = (number % 2)
-if (remainder == 0){
-    console.log('${number} is even')
-}else{
-    console.log('${number} is odd')
+//let number = 6
+//let remainder = number % 2
+//if (remainder == 0){
+//    console.log('${number} is even')
+//}else{
+ //   console.log('${number} is odd')
+//}
+function checkifeven(num) {
+    let remainder = num % 2
+    if (remainder == 0){
+           console.log('${num} is even')
+        }else{
+           console.log('${num} is odd')
+        }
 }
+checkifeven(76)
+checkifeven(50987)
+checkifeven(787)
